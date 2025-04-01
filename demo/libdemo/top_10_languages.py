04c083a42dc2edf63a860b16c74e23c485018b0e
srikanthpragada <srikanthpragada@gmail.com>
2025-04-01 19:54:54 +0530

    01-APR

--- demo/libdemo/top_10_languages.py
+++ demo/libdemo/top_10_languages.py
@@ -6,6 +6,27 @@ contents = response.text
 
 soup = BeautifulSoup(contents, 'html.parser')
 top20_table = soup.find(id="top20")
-print(top20_table.text)
+
+if top20_table is None:
+    print('Sorry! Could not find top20 table!')
+    exit()
+
+
+#print(top20_table.text)
+table_body = top20_table.find("tbody")
+#print(table_body)
+
+rows  = table_body.find_all("tr")
+for row in rows[:10]:
+    cols = row.find_all("td")
+    name = cols[4].text
+    rank = float(cols[5].text.strip("%"))
+    print(f"{name:25} {rank:5.2f}%")
+
+
+
+
+
+
 
 
